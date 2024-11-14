9a562a52db681dbacdc2fa67de08bdb0f67ff89c
Cmrdeveloper <cmrbolsadevelopment@gmail.com>
2024-11-14 12:28:45 +0000

    banda

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,7 +12,7 @@ const containerStyle = {
 
 const Header = () => {
   const calculateTimeLeft = () => {
-    const eventDate = new Date("2024-11-11T23:59:59"); // Ajusta la fecha del evento
+    const eventDate = new Date("2024-11-17T23:59:59"); // Ajusta la fecha del evento
     const currentTime = new Date();
     const difference = eventDate - currentTime;
 
--- src/components/PreciosTalleres.jsx
+++ src/components/PreciosTalleres.jsx
@@ -3,7 +3,7 @@ import { FaWhatsapp } from "react-icons/fa";
 
 
 const handleCharlaClick = () => {
-  window.location.href = 'https://mentoriapev.com/finalizar-compra/?add-to-cart=5911';
+  window.location.href = 'https://mentoriapev.com/finalizar-compra/?add-to-cart=5910';
 };
 
 const handleTallerClick = () => {
@@ -23,35 +23,39 @@ const PreciosTalleres = () => {
       </div>
 
       <div className="md:flex md:flex-row md:justify-center md:gap-5 flex flex-col gap-5 md:ml-16 md:mr-16 ml-5 mr-5 ite">
-        {/* Primera tabla */}
-        <div className="border-2 border-black ">
-          <div className="text-center">
-            <h1 className="md:text-3xl text-2xl font-bold m-5 text-center">CHARLA GRATUITA 29 NOVIEMBRE</h1>
-            <h1 className="flex justify-center items-center font-semibold text-white bg-cmrbolsa md:text-2xl text-md">Charla GRATUITA</h1>
-            <h5 className="text-lg bg-cmrbolsa text-white md:h-9 h-20 p-2 md:pb-9 text-center">Se abona <span className="text-black font-bold">5 Euros</span> que se devolveran el día del evento al presentarte en la charla</h5>
-            <h2 className="md:text-7xl text-5xl font-bold text-cmrbolsa m-5">
-            <h2 className="text-xl font-bold text-cmrbolsa">Reserva tú plaza</h2>
-              <span className="text-black text-lg vertical-align-top ml-5 "></span>
-              5,00
-              <span className="text-2xl text-cmrbolsa font-bold"> €</span>
-            </h2>
-            <h5 className="mt-2 font-semibold md:mb-28 w-full max-w-xl mx-auto text-center">
-            Este importe <span className="text-cmrbolsa text-2xl font-bold">se devolverá</span> el día del evento al presentarte en la charla
-            </h5>
-          </div>
-          <div className="flex flex-col items-center gap-5">
-            <h5 className="text-2xl text-white font-semibold bg-cmrbolsa w-full p-2 text-center">¿Qué incluye?</h5>
-            <h3 className="flex items-center md:gap-2 gap-1"><FaCheck />Aprende a leer los gráficos</h3>
-            <h3 className="flex items-center md:gap-2 gap-1"><FaCheck />Curso básico de bolsa</h3>
-            <h3 className="flex items-center md:gap-2 gap-1"><FaCheck />3 Sorteo de un mes de sala gratis</h3>
 
+  {/* Primera tabla */}
+<div className="relative border-2 border-black overflow-hidden">
+  {/* Banda diagonal */}
+  <div className="absolute -rotate-45 bg-red-500 text-white font-bold md:text-5xl py-9 px-10 top-1/2 left-[-50%] w-[200%] text-center z-20" style={{ letterSpacing: '0.9em' }}>
+      COMPLETADO
+    </div>
 
-            <button onClick={handleCharlaClick} className="boton_reserva boton_animation bg-cmrbolsa uppercase">
-              Me apunto
-            </button>
-          </div>
-        </div>
+  <div className="text-center relative z-10">
+    <h1 className="md:text-3xl text-2xl font-bold m-5 text-center">CHARLA GRATUITA 29 NOVIEMBRE</h1>
+    <h1 className="flex justify-center items-center font-semibold text-white bg-cmrbolsa md:text-2xl text-md">Charla GRATUITA</h1>
+    <h5 className="text-lg bg-cmrbolsa text-white md:h-9 h-20 p-2 md:pb-9 text-center">Se abona <span className="text-black font-bold">5 Euros</span> que se devolveran el día del evento al presentarte en la charla</h5>
+    <h2 className="md:text-7xl text-5xl font-bold text-cmrbolsa m-5">
+      <h2 className="text-xl font-bold text-cmrbolsa">Reserva tú plaza</h2>
+      <span className="text-black text-lg vertical-align-top ml-5 "></span>
+      5,00
+      <span className="text-2xl text-cmrbolsa font-bold"> €</span>
+    </h2>
+    <h5 className="mt-2 font-semibold md:mb-28 w-full max-w-xl mx-auto text-center">
+      Este importe <span className="text-cmrbolsa text-2xl font-bold">se devolverá</span> el día del evento al presentarte en la charla
+    </h5>
+  </div>
 
+  <div className="flex flex-col items-center gap-5 relative z-10">
+    <h5 className="text-2xl text-white font-semibold bg-cmrbolsa w-full p-2 text-center">¿Qué incluye?</h5>
+    <h3 className="flex items-center md:gap-2 gap-1"><FaCheck />Aprende a leer los gráficos</h3>
+    <h3 className="flex items-center md:gap-2 gap-1"><FaCheck />Curso básico de bolsa</h3>
+    <h3 className="flex items-center md:gap-2 gap-1"><FaCheck />3 Sorteo de un mes de sala gratis</h3>
+    <button onClick={handleCharlaClick} className="boton_reserva boton_animation bg-cmrbolsa uppercase">
+      Ir al Taller
+    </button>
+  </div>
+</div>
         {/* Segunda tabla */}
         <div className="border-2 border-black ">
           {/* <div className="">
@@ -120,7 +124,7 @@ const PreciosTalleres = () => {
     <div className="flex flex-col text-center">
       <h2 className="text-xl font-bold text-cmrbolsa mt-5">Ahora</h2>
       <h2 className="md:text-7xl text-5xl font-bold text-cmrbolsa text-center">
-        225,00
+        260,00
         <span className="text-2xl text-cmrbolsa font-bold"> €</span>
       </h2>
     </div>
@@ -128,8 +132,8 @@ const PreciosTalleres = () => {
     {/* Precios tachados */}
     <h2 className="md:text-xl text-md font-bold text-cmrbolsa text-center ml-5 mr-5 mt-1">
       <span className="line-through text-gray-300">195,00 €</span>
-      <span className="ml-3 text-cmrbolsa ">225,00 €</span>
-      <span className="ml-3 line-through text-gray-300">260,00 €</span>
+      <span className="ml-3 line-through text-gray-300 ">225,00 €</span>
+      <span className="ml-3 text-cmrbolsa">260,00 €</span>
       <span className="ml-3 line-through text-gray-300">295,00 €</span>
       <span className="ml-3 line-through text-gray-300">325,00 €</span>
       <span className="ml-3 line-through text-gray-300">380,00 €</span>
@@ -138,7 +142,7 @@ const PreciosTalleres = () => {
 
     <h5 className="mt-2 font-semibold mb-5 w-full text-center">
       <h5 className="mt-2 font-semibold mb-5 w-full max-w-xl mx-auto text-center">
-        Si te apuntas antes de que términe el contador pagas <span className="text-cmrbolsa text-2xl font-bold">SOLO 225 €</span>
+        Si te apuntas antes de que términe el contador pagas <span className="text-cmrbolsa text-2xl font-bold">SOLO 260 €</span>
       </h5>
       <h5 className="mt-2 font-semibold mb-5 w-full max-w-xl mx-auto text-center">
         Cada semana aumentará el precio así que cuanto antes te apuntes menos pagarás
